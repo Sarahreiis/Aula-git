c4ee6259a989f89fcbd4106934929b0bef7e5aeb
SARAH BARROS REIS <sarah.reis5@senaisp.edu.br>
2026-03-18 15:07:02 -0300

    feat(ex01): cadastro de aluno.

---
+++ src/ex01.java
@@ -0,0 +1,20 @@
+void main() {
+
+    Scanner sc = new Scanner (System.in);
+    IO.println("Digite seu nome");
+    String nome = sc.next();
+
+    IO.println("Qual curso que você está matriculada");
+    String curso = sc.next();
+
+    IO.println("Qual é a sua idade?");
+    int idade = sc.nextInt();
+
+    IO.println("Informações do aluno");
+    IO.println("seu nome é " +nome);
+    IO.println("seu curso é " +curso);
+    IO.println("sua idade é " +idade);
+
+
+
+}
