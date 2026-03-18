0e2df16ab2f2bc5a65ce6d9e90f10095db399575
SARAH BARROS REIS <sarahreis895@gmail.com>
2026-03-18 16:31:09 -0300

    feat(ex03) : perfil gamer

---
+++ src/Ex03.java
@@ -0,0 +1,24 @@
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+    IO.println("Nome (nickname) do jogador");
+    String nome = sc.nextLine();
+
+    IO.println("Seu jogo favorito");
+    String jogo = sc.nextLine();
+
+    IO.println("Pontuação atual");
+    double pontuacao = sc.nextDouble();
+
+    IO.println("informações do perfil gamer");
+    IO.println("seu nome é " + nome);
+    IO.println("seu jogo favorito é " + jogo);
+    IO.println("pontuação atual do jogo " + pontuacao);
+
+
+
+    }
+
+
+
+
