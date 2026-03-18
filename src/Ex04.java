a0258950b92a4427e58f0adaf3524c4f9da62a04
SARAH BARROS REIS <sarahreis895@gmail.com>
2026-03-18 17:11:18 -0300

    feat(ex04) : meta de passos

---
+++ src/Ex04.java
@@ -0,0 +1,29 @@
+void main() {
+    Scanner sc = new Scanner(System.in);
+    int passos, passosrestantes, passosextras, passosmeta;
+
+    passosmeta = 10000;
+
+    IO.println("Digite quantos passos vc deu hoje ao total");
+    passos = sc.nextInt();
+
+    if (passos == passosmeta) {
+        IO.println("Você atingiu sua meta");
+
+
+    } else if (passos < passosmeta) {
+        IO.println("você não atingiu sua meta faltam : ");
+        passosrestantes = passosmeta - passos;
+        IO.println("faltam " + passosrestantes);
+
+    }else {
+        IO.println("Você ultrapassou sua meta : ");
+        passosrestantes
+
+
+
+    }
+
+
+
+}
