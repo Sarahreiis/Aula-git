c14b2857115aa207eb2c552591bdece8afdce28a
SARAH BARROS REIS <sarahreis895@gmail.com>
2026-03-25 15:20:46 -0300

    feat.(ex06)

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,12 +1,66 @@
-import java.util.Scanner;
+void main () {
 
-public class Ex06 {
-    static void main() {
-        new Scanner(System.in);
-        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+    Scanner sc = new Scanner(System.in);
+    int estudar, horasrestantes, horasextras, estudarmeta;
+
+    estudarmeta = 40;
+
+    IO.println(" quantas horas você estudou hoje?");
+    estudar = sc.nextInt();
+
+    if (estudar == estudarmeta) {
+        IO.println("Você conseguiu atingir seu objetivo");
+
+
+    } else if (estudar < estudarmeta) {
+        IO.println("você não atingiu seu objetivo faltam : ");
+        horasrestantes = estudarmeta - estudar;
+        IO.println("faltam " + horasrestantes);
+
+    }else{
+
+        IO.println("Você ultrapassou sua meta : ");
+        IO.println(estudar - 40);
+
 
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
